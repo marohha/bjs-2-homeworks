8020761fb716d078d196125e62eb6633c299f299
Marohha <marohha@yandex.ru>
2023-09-11 18:12:47 +0300

    First homework edit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -3,9 +3,7 @@ function solveEquation(a, b, c) {
   let arr = [];
   let d = b**2 - 4 * a * c;
   
-  if (d < 0) {
-    arr = [];
-  } else if (d === 0) {
+  if (d === 0) {
     let root = (-b / (2 * a));
     arr.push(root);
   } else if (d > 0) {
