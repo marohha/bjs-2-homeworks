7159af0477979cfc5b54e2a9e1e5bce81bce67a7
Marohha <marohha@yandex.ru>
2023-09-18 19:19:39 +0300

    homeWork Help

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,30 @@
 function Student(name, gender, age) {
-  
+  this.name = name,
+  this.gender = gender,
+  this.age = age,
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function (...marksToAdd) {
+  if ("marks" in Student) {
+    Student.marks.push(...marksToAdd);
+  }
 }
 
 Student.prototype.getAverage = function () {
-  
+  if ("marks" in Student && Student.marks.length > 0) {
+    return Student.marks.reduce((acc, item) => acc + item / Student.marks.length, 0);
+  } else {
+    return 0;
+  }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
 }
