6cb8892fdbe0ed519c643fb9bcfaf7f35f59400c
Marohha <marohha@yandex.ru>
2023-09-29 09:32:30 +0300

    HomeWork complete

--- 7.async/task.js
+++ 7.async/task.js
@@ -9,45 +9,42 @@ class AlarmClock {
             throw new Error('Отсутствуют обязательные аргументы');
         }
 
-        if (time in this.alarmCollection) {
+        if (this.alarmCollection.some(item => {
+            return item === time;
+        })) {
             console.warn('Уже присутствует звонок на это же время');
         }
 
         let canCall = true;
-        callback(() => canCall);
         this.alarmCollection.push({callback, time, canCall});
     }
 
     removeClock(timeNew) {
-        for (let i = 0; i < this.alarmCollection.length; i++) {
-            if (this.alarmCollection[i].time === timeNew) {
-                this.alarmCollection.splice(timeNew, 2);
-            }
-        }
-        // delete this.alarmCollection.filter(item => item.time === timeNew); 
+        this.alarmCollection = this.alarmCollection.filter(item => item.time != timeNew); 
     }
 
     getCurrentFormattedTime() {
-        return String(new Date().toLocaleTimeString("ru-Ru", {
+        return new Date().toLocaleTimeString("ru-Ru", {
             hour: "2-digit",
             minute: "2-digit",
-          }));
+          });
     }
 
     start() {
-        if (!this.intervalId === null) {
+        if (this.intervalId != null) {
             return;
         }
-        setInterval(() => this.alarmCollection.forEach((alarm) => {
-            if (this.time === this.getCurrentFormattedTime() && alarm.canCall === true) {
+        this.intervalId = setInterval(() => this.alarmCollection.forEach((alarm) => {
+            if (alarm.time === this.getCurrentFormattedTime() && alarm.canCall === true) {
                 alarm.canCall = false;
-                callback();
+                alarm.callback();
             }
         }), 1000);
     }
 
     stop() {
-        clearInterval(() => this.intervalId = null);
+        clearInterval(this.intervalId);
+        this.intervalId = null;
     }
 
     resetAllCalls() {
@@ -58,7 +55,4 @@ class AlarmClock {
         this.stop(); 
         this.alarmCollection = [];
     }
-
-
-
 }
